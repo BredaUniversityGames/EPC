6197c74e01a90db1991ff5094fa9927cda9e290c
ArchBacon <C.Delicaat@hotmail.com>
2022-11-22 00:51:14 +0100

    Started on pricing table

---
+++ assets/controllers/pricing_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = [ "overlay", "menu", "closeButton" ];
+
+    buasStudents() {
+
+    }
+
+    otherStudents() {
+
+    }
+
+    alumni() {
+
+    }
+
+    industry() {
+
+    }
+}
