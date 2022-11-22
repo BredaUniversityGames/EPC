2c503516a388bb66a026144736941ba9fa64bfba
ArchBacon <C.Delicaat@hotmail.com>
2022-11-22 21:25:26 +0100

    Started on masterclass section

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,9 @@ module.exports = {
                     900: '#373e42',
                 },
             },
+            boxShadow: {
+                'around': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)'
+            }
         },
     },
     plugins: [],
