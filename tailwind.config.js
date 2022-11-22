74d9d165c4e50bb23b0bf9b9925c12962d3709e2
ArchBacon <C.Delicaat@hotmail.com>
2022-11-22 22:56:24 +0100

    Changed dark theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,6 @@ module.exports = {
     darkMode: 'class',
     theme: {
         extend: {
-            colors: {
-                'nevada': {
-                    DEFAULT: '#60777f',
-                    50: '#f4f7f7',
-                    100: '#e3e8ea',
-                    200: '#cad5d7',
-                    300: '#a5b6bb',
-                    400: '#799097',
-                    500: '#60777f',
-                    600: '#50626a',
-                    700: '#455259',
-                    800: '#3e474c',
-                    900: '#373e42',
-                },
-            },
             boxShadow: {
                 'around': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)'
             }
