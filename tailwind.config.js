d95debbcdeb174d36cd3813b175aeedf6879345c
ArchBacon <C.Delicaat@hotmail.com>
2022-12-06 15:15:59 +0100

    Updated header navigation responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         extend: {
             boxShadow: {
                 'around': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)'
-            }
+            },
         },
     },
     plugins: [],
