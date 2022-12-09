eda2ce6c77daf5f067603b9413868bf4c2afb71d
ArchBacon <C.Delicaat@hotmail.com>
2022-12-09 01:39:36 +0100

    Added newsletter, socials, and footer. Overhauled classes, containerized sections, added content to match current site.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
             boxShadow: {
                 'around': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)'
             },
+            minHeight: {
+                '16': '4rem',
+            },
         },
     },
     plugins: [],
