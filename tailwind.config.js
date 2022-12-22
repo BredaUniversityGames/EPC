ba62d8d78dda2f013d9cedb561ecd7b8e4b99f83
Bojan Endrovski <bojan.endrovski@gmail.com>
2022-12-22 11:28:23 +0100

    First blue version of the EPC website

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,22 @@ module.exports = {
     ],
     darkMode: 'class',
     theme: {
+        fontFamily : {
+            'sharetech' : [ 'Share Tech Mono', 'monospace' ],
+        },        
         extend: {
             boxShadow: {
                 'around': '0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1)'
             },
             minHeight: {
                 '16': '4rem',
+            },
+            colors: {
+                'epcblue': '#00C3E3',
+            },
+            fontSize: {    
+                'huge': '8.38rem',        
+                'damn': '18.5rem',
             },
         },
     },
